68c3f9b34b2b06b0295f294c44751030884369fe
Neto <yasuoxd1v9@gmail.com>
2025-07-01 14:49:42 -0300

    auth modules

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,4 +1,24 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
+import { AuthService } from './auth.service';
+import { RegisterUserDto } from './dto/register.dto';
+import {
+  ApiBody,
+  ApiConflictResponse,
+  ApiCreatedResponse,
+} from '@nestjs/swagger';
 
 @Controller('auth')
-export class AuthController {}
+export class AuthController {
+  constructor(private authService: AuthService) {}
+  @ApiBody({ type: RegisterUserDto })
+  @ApiCreatedResponse({
+    description: 'Usuário registrado com sucesso!',
+  })
+  @ApiConflictResponse({
+    description: 'Usuário já cadastrado.',
+  })
+  @Post('register')
+  async register(@Body() userData: RegisterUserDto) {
+    return this.authService.registerUser(userData);
+  }
+}
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,38 @@
-import { Injectable } from '@nestjs/common';
+import { ConflictException, Injectable } from '@nestjs/common';
+import { JwtService } from '@nestjs/jwt';
+import { PrismaService } from 'src/prisma/prisma.service';
+import { RegisterUserDto } from './dto/register.dto';
+import * as bcrypt from 'bcrypt';
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+  constructor(
+    // private jwt: JwtService,
+    private prisma: PrismaService,
+  ) {}
+
+  async registerUser(UserData: RegisterUserDto) {
+    const userExists = await this.prisma.user.findUnique({
+      where: { email: UserData.email },
+    });
+    if (userExists) {
+      throw new ConflictException('Usuário ja cadastrado');
+    }
+    const hashedPassword = await bcrypt.hash(UserData.password, 10);
+
+    const newUser = await this.prisma.user.create({
+      data: {
+        name: UserData.name,
+        email: UserData.email,
+        password: hashedPassword,
+      },
+      select: {
+        id: true,
+        name: true,
+        email: true,
+        role: true,
+      },
+    });
+    return newUser;
+  }
+}
